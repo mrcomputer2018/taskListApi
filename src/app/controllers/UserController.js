15d8525e2d339282e2b87d9e267aa598a6c01c29
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-06-10 04:35:22 -0300

    Feat: cadastro de usuarios

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+
+class UserController {
+    // eslint-disable-next-line consistent-return
+    async store(req, res) {
+        try {
+            const userExists = await User.findOne({
+                Where: { email: req.body.email },
+            });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'Usuario já existe.' });
+            }
+
+            // desconstruindo para devolver somente o necessario
+            const { id, name, email } = await User.create(req.body);
+
+            return res.json({ id, name, email });
+        } catch (error) {
+            console.log({ error: error.message });
+        }
+    }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-    try {
-        const user = await User.create({
-            name: 'marcelo ribeiro',
-            email: 'marcelo@marcelo.com',
-            password_hash: '123123',
-        });
-
-        res.json(user);
-    } catch (error) {
-        console.log({ error: error.message });
-    }
-});
+routes.post('/users', UserController.store);
 
 export default routes;
